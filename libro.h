f13d2d522c2720ada76002e922e13339b9fb1c96
Isidora Lin <101031586+isi-lin@users.noreply.github.com>
2022-04-03 21:08:22 -0400

    Struct libros
    Definición de parametros

---
+++ libro.h
@@ -0,0 +1,11 @@
+typedef struct libros{
+    char titulo[50];
+    char autor[50];
+    int anio;
+    int estante_numero;
+    char estante_seccion[50];
+    int piso;
+    char edificio[4];
+    char sede[50];
+    int borrar_libro;
+}biblioteca;
